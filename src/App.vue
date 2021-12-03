f1dfc22070bece1752f1d44844e0b8374aa2a4d8
Vivek Singh <vivek20.singh@paytm.com>
2021-12-03 22:04:05 +0530

    intial setup

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <Header />
+  <Home />
+  <Footer />
+</template>
+
+<script>
+import Header from './components/Header'
+import Home from './components/Home'
+import Footer from './components/Footer'
+
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Home,
+    Footer
+  }
+}
+</script>
+
+<style src="./index.css" />
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul class="text-right p-4 font-semibold bg-gray-100 pr-4">
+      hello
+  </ul>
+</template>
+
+<script>
+export default {
+ name: 'Navbar',
+ props: {
+    links: [String]
+ },
+}
+</script>
+
+<style>
+
+</style>
