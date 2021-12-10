8a4513c3cad89f742471c1ec53bc6adde09c17c2
Vivek Singh <viveksinghup.singh@gmail.com>
2021-12-10 09:12:25 +0530

    homep

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,16 +1,8 @@
 <template>
-  <div class="bg-opacity-40 bg-gray10">
+  <div class="bg-opacity-50 bg-gray10">
     <div class="container mx-auto px-4">
       <div class="flex pt-48 pb-28">
-        <div
-          class="
-            flex
-            items-center
-            text-center
-            lg:text-left
-            lg:w-1/2 pr-20
-          "
-        >
+        <div class="flex items-center text-center lg:text-left lg:w-1/2 pr-20">
           <div>
             <h2 class="text-4xl font-semibold md:text-7xl">
               Your Facility Experts
@@ -44,16 +36,74 @@
           </div>
         </div>
         <div class="lg:w-1/2 pl-32">
-          <div class=" w-80 h-full bg-darkBlue"></div>
+          <div class="w-80 h-full bg-darkBlue"></div>
         </div>
       </div>
     </div>
   </div>
+  <div class="my-20 relative overflow-hidden">
+    <div class="container mx-auto md:px-20">
+      <div class="flex md:flex-row items-center flex-col">
+        <div class="md:w-1/2">
+          <div class="text-5xl md:pr-32 form-heading">
+            Get a free consultation from top industry professionals and manage
+            your facility with ease and perfection.
+          </div>
+          <div class="h-2 w-16 bg-darkBlue mt-5 mb-4"></div>
+        </div>
+        <div class="md:w-1/2">
+          <EnquiryForm></EnquiryForm>
+        </div>
+      </div>
+    </div>
+    <div
+      class="
+        max-w-md
+        absolute
+        top-0 right-0
+        sideimg sm:rounded-l-3xl
+        bg-brandBlue
+        p-10 hidden md:block
+        h-full
+        text-white
+      "
+    ></div>
+  </div>
+  <div
+    class="
+      container
+      mx-auto
+      px-4
+      border-t border-gray10 border-opacity-50
+      py-10
+      my-10
+    "
+  >
+    <div class="flex flex-col items-center">
+      <div class="text-4xl font-medium">Our Clients</div>
+      <div class="h-1 w-16 bg-darkBlue mt-5 mb-4"></div>
+    </div>
+    <div class="flex justify-center">
+      <img src="@/assets/images/clients.png" />
+    </div>
+  </div>
+  <section>
+    <img src="@/assets/images/home-coverimg.png" class="w-full h-auto" />
+  </section>
 </template>
 
 <script>
-export default {};
+import EnquiryForm from "../common/EnquiryForm";
+export default {
+  name: "Home",
+  components: {
+    EnquiryForm,
+  },
+};
 </script>
 
-<style>
+<style lang="postcss" scoped>
+.form-heading {
+  line-height: 60px;
+}
 </style>
