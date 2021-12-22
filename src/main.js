821afab50985acc5fa3d2c4142877670fc4591fd
Prashant Mishra <prashant.mishra@traderinteractive.com>
2021-12-22 17:04:41 +0530

    toster ui updated

--- src/main.js
+++ src/main.js
@@ -4,5 +4,11 @@ import './index.css'
 import router from './router/index'
 import DKToast from 'vue-dk-toast';
 
-createApp(App).use(DKToast).use(router).mount('#app')
+createApp(App).use(DKToast, {
+    positionX: 'right',
+    positionY: 'top',
+    styles: {
+             height: '50px',
+            },
+}).use(router).mount('#app')
 
